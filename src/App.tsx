d65fb8d19221272b3195084877c5233ebc0fc415
Simon <sleepdefic1t@users.noreply.github.com>
2022-02-16 20:07:09 -0500

    chore: improve code and design

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import * as Bip44 from "./bip44";
 export default function App() {
 	const [path, setPath] = useState("");
 	const [parsed, setParsed] = useState("");
-	const pathPlaceholder = "BIP Path (44'/1'/0'/0/0)";
 
 	function handleParse() {
 		try {
@@ -17,25 +16,60 @@ export default function App() {
 		setPath("");
 	}
 
+	function setDefault() {
+		setPath(defaultPath);
+	}
+
+	const headerImageUrl = "https://raw.githubusercontent.com/LedgerHQ/ledger-live-desktop/develop/build/icon.png"
+	const headerText = "This tool parses a standard BIP-44 path into a packed uint32 hex-string prepended by its element length. This is useful when creating APDU instructions for Ledger HW wallet communication where a derivation path is required.";
+
+	const defaultPath = "44'/1'/0'/0/0"
+	const defaultResult ="058000002c80000001800000000000000000000000";
+	const pathPlaceholder = "Path: (e.g., \"" + defaultPath + "\")";
+
+	function ContentHeader() {
+		return (
+			<header className="content-header">
+				<img src={headerImageUrl} />
+				<p className="header-description">{headerText}</p>
+			    <p className="header-example">
+				    For example, the path <code>{defaultPath}</code> becomes <code>{defaultResult}</code>.
+			    </p>
+			</header>
+		)
+	}
+
+	function ContentBody() {
+		return (
+			<div className="content-body">
+				<input className={path}
+					   type="text"
+					   placeholder={pathPlaceholder}
+					   defaultValue={path}
+					   onChange={e => setPath(e.target.value)} />
+			    <button className="default-button"
+					    onClick={setDefault}>Use Default</button>
+				<button className="parse-button"
+					    onClick={handleParse}>Parse Path</button>
+			</div>
+		)
+	}
+
+	function ContentFooter() {
+		return (
+			<footer className="content-result">
+				<textarea className={parsed}
+					   defaultValue={parsed}
+					   placeholder={"result"} />
+			</footer>
+		)
+	}
+
 	return (
 		<main>
-			<header>
-				<center>
-					<img src="https://raw.githubusercontent.com/LedgerHQ/ledger-live-desktop/develop/build/icon.png" width="250px" />
-				</center>
-				<p style={{fontSize: '10px', textAlign: 'left'}}>This tool parses a standard BIP-44 path into a packed uint32 hex-string prepended by its element length. This is useful when creating APDU instructions for Ledger HW wallet communication where a derivation path is required.</p>
-				<p style={{fontSize: '10px'}}>For example, the path <code style={{background: 'rgba(0, 0, 0, 0.25)'}}>"44'/1'/0'/0/0"</code> becomes <code style={{background: 'rgba(0, 0, 0, 0.25)'}}>058000002c80000001800000000000000000000000</code>.</p>
-			</header>
-			<div>
-				<div>Enter your BIP Path below:</div>
-				<input label="Path" placeholder={pathPlaceholder} value={path} onChange={e => setPath(e.target.value)} className={path} />
-			</div>
-			<div>
-				<button style={{ width: '50%', color: 'black' }} onClick={handleParse}>Parse Path</button>
-			</div>
-			<result>
-				<div>Parsed Path: {parsed}</div>
-			</result>
+			<ContentHeader />
+			<ContentBody />
+			<ContentFooter />
 		</main>
 	)
 }
