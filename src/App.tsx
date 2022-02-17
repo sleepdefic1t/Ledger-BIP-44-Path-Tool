727c44639f9bea3295bb59cea5ec18a35a3187c2
Simon <sleepdefic1t@users.noreply.github.com>
2022-02-16 22:18:02 -0500

    chore: update code

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import './App.css'
 import * as Bip44 from "./bip44";
 
 export default function App() {
+	const headerImageUrl = "https://raw.githubusercontent.com/LedgerHQ/ledger-live-desktop/develop/build/icon.png"
+	const headerText = "This tool parses a standard BIP-44 path into a packed uint32 hex-string prepended by its element length. This is useful when creating APDU instructions for Ledger HW wallet communication where a derivation path is required.";
+
+	const defaultPath = "44'/1'/0'/0/0"
+	const defaultResult ="058000002c80000001800000000000000000000000";
+	const pathPlaceholder = "Path: (e.g., \"" + defaultPath + "\")";
+
 	const [path, setPath] = useState("");
 	const [parsed, setParsed] = useState("");
 
@@ -20,15 +27,8 @@ export default function App() {
 		setPath(defaultPath);
 	}
 
-	const headerImageUrl = "https://raw.githubusercontent.com/LedgerHQ/ledger-live-desktop/develop/build/icon.png"
-	const headerText = "This tool parses a standard BIP-44 path into a packed uint32 hex-string prepended by its element length. This is useful when creating APDU instructions for Ledger HW wallet communication where a derivation path is required.";
-
-	const defaultPath = "44'/1'/0'/0/0"
-	const defaultResult ="058000002c80000001800000000000000000000000";
-	const pathPlaceholder = "Path: (e.g., \"" + defaultPath + "\")";
-
-	function ContentHeader() {
-		return (
+	return (
+		<main>
 			<header className="content-header">
 				<img src={headerImageUrl} />
 				<p className="header-description">{headerText}</p>
@@ -36,40 +36,23 @@ export default function App() {
 				    For example, the path <code>{defaultPath}</code> becomes <code>{defaultResult}</code>.
 			    </p>
 			</header>
-		)
-	}
-
-	function ContentBody() {
-		return (
 			<div className="content-body">
 				<input className={path}
 					   type="text"
 					   placeholder={pathPlaceholder}
 					   defaultValue={path}
+					autoFocus
 					   onChange={e => setPath(e.target.value)} />
 			    <button className="default-button"
 					    onClick={setDefault}>Use Default</button>
 				<button className="parse-button"
 					    onClick={handleParse}>Parse Path</button>
 			</div>
-		)
-	}
-
-	function ContentFooter() {
-		return (
 			<footer className="content-result">
 				<textarea className={parsed}
 					   defaultValue={parsed}
 					   placeholder={"result"} />
 			</footer>
-		)
-	}
-
-	return (
-		<main>
-			<ContentHeader />
-			<ContentBody />
-			<ContentFooter />
 		</main>
 	)
 }
